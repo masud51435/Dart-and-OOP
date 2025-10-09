8e11011fbafd323bcbc22ac6ccc942b089762a24
MD MASUD RANA <86624203+masud51435@users.noreply.github.com>
2025-10-09 11:55:48 +0600

    added solid principle

---
+++ solid_principle/solid.dart
@@ -0,0 +1,7 @@
+// solid principle are the set of guidelines for writing clean, maintainable and scalable object oriented code. by applying those principle to your flutter development , you can create well structured and maintainable application that are easy to test, maintain, understand and extend in future.
+
+// s ------ single responsibility principle (SRP)
+// o ------ open/closed principle (OCP)
+// l ------ liskov substitution principle (LSP)
+// i ------ interface segregation principle (ISP)
+// d ------ dependency inversion principle (DIP)
